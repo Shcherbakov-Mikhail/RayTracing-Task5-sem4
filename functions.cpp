a65395bb2ccb9b2221a8dbf8fecbbec5cd3ad754
Shcherbakov Mikhail <shcherbakov.o.e@gmail.com>
2020-05-23 06:05:24 +0300

    Small improvement of coloring function

--- functions.cpp
+++ functions.cpp
@@ -82,12 +82,7 @@ Vec3f castRay(
     int index = 0;
     Object *hitObject = nullptr;
     if (trace(orig, dir, objects, tnear, index, uv, &hitObject, options)){
-        if (options.num_objects == 1){
-            hitColor = Vec3f(64, 64, 64);
-        }
-        else{
-            hitColor = Vec3f(64, 64, 64) + Vec3f(static_cast<int>(127/(options.num_objects-1))) * (hitObject->pos-1);
-        }
+        hitColor = Vec3f(64, 64, 64) + Vec3f(static_cast<int>(127/(options.num_objects-1))) * (hitObject->pos-1);
     }
     return hitColor;
 }
